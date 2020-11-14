cea015e279c5ee17af056ef0201cb146088d8909
unknown <phamquoctoan11081999@gmail.com>
2020-11-14 15:55:00 +0700

    (1.0.0)

---
+++ server.js
@@ -0,0 +1,13 @@
+// console.log('chao tui mat loln'
+var app = require("express")();
+var port = 8080;
+app.listen(port, (err) => {
+    if (err) {
+        console.log('dmm tui may');
+    } else {
+        console.log('ahihi')
+    }
+})
+app.get("/", (req, res) => {
+    res.json({ "mes": "dmmm" });
+});
